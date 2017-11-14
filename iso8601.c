b5db3c8cde2a242a03df211d7065a26b1936a7ac
Alexandr SARANDI <asarandi@e1z2r4p25.42.us.org>
2017-11-14 13:45:54 -0800

    type %k bug fix; final submit

--- iso8601.c
+++ iso8601.c
@@ -6,7 +6,7 @@
 /*   By: asarandi <asarandi@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/11 18:06:01 by asarandi          #+#    #+#             */
-/*   Updated: 2017/11/14 01:00:12 by asarandi         ###   ########.fr       */
+/*   Updated: 2017/11/14 13:43:52 by asarandi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	iso_time(uintmax_t t)
 	int			leaps;
 	int			is_leap;
 	int			day;
-	const int	months[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	static int	months[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	int			mo;
 
 	leaps = 0;
@@ -29,12 +29,11 @@ void	iso_time(uintmax_t t)
 	day = ((t / 86400) - (leaps - is_leap)) % 365;
 	day += 1;
 	mo = 0;
+	months[1] = 28;
+	if (is_leap)
+		months[1] = 29;
 	while (day > months[mo])
-	{
 		day -= months[mo++];
-		if ((is_leap) && (mo == 1))
-			day -= 1;
-	}
 	leaps = (leaps - is_leap) * 86400;
 	ft_printf("%d-%02d-%02d", ((t - leaps) / 31536000) + 1970, ++mo, day);
 	ft_printf("T%02d:%02d:%02d+00:00", t / 3600 % 24, t / 60 % 60, t % 60);
